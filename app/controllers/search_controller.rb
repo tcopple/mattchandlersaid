9ea209d844a869c36291716c511b6d78c9b79503
tcopple <J.TylerCopple@gmail.com>
2012-02-24 10:43:28 -0500

    Fixing some tabbing and removing unnecessary spaces.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,24 +17,22 @@ class SearchController < ApplicationController
       q.update_attributes(:count => q.count+1, :last_use => DateTime.now)
     end
 
-    @results = Sermon.search params[:search], 
+    @results = Sermon.search params[:search],
 		 	     :with => {:published_date => (DateTime.new(2006, 1, 1)..DateTime.now)},
-			     :page => page, 
-			     :per_page => 10, 
-                             :field_weights => { :title => 10, :content => 5}
+			     :page => page,
+			     :per_page => 10,
+           :field_weights => { :title => 10, :content => 5}
 
     respond_with @results
   end
 
   def recent
     @sermons = Sermon.where("sermons.published_date >= '#{2.months.ago}'").order("sermons.published_date desc")
-
     respond_with @sermons
   end
 
   def queries
     @queries = Query.where("queries.last_use >= '#{1.month.ago}'").order("queries.count desc").limit(5)
-
     respond_with @queries
   end
 end
